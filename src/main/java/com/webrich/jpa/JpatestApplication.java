3fe3c011c828ac49119334fbd8cb4e43eb3ed74f
Eknath Kadam <eknathkadam@gmail.com>
2022-05-22 17:18:40 +0530

    Delete of CRUD using JPAEntity manager

--- src/main/java/com/webrich/jpa/JpatestApplication.java
+++ src/main/java/com/webrich/jpa/JpatestApplication.java
@@ -12,34 +12,39 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpatestApplication {
 
 	public static void main(String[] args) {
-		
+
 		SpringApplication.run(JpatestApplication.class, args);
 
-		// Employee emp1 = new Employee(1, "eknath", 42, "610553594", new Date(),EmployeeType.CONTRACTOR);
-		
-		// Employee emp2 = new Employee(2, "Girish", 28, "610553595",new Date(),EmployeeType.FULL_TIME);
-		// Employee emp3 = new Employee(3, "Nirav", 45, "610553596",new Date(),EmployeeType.PAYROLL_EXEMPT);
+		// Employee emp1 = new Employee(1, "eknath", 42, "610553594", new
+		// Date(),EmployeeType.CONTRACTOR);
+
+		// Employee emp2 = new Employee(2, "Girish", 28, "610553595",new
+		// Date(),EmployeeType.FULL_TIME);
+		// Employee emp3 = new Employee(3, "Nirav", 45, "610553596",new
+		// Date(),EmployeeType.PAYROLL_EXEMPT);
 
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myApp");
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 
 		Employee emp = entityManager.find(Employee.class, 1);
 
-		 EntityTransaction transaction = entityManager.getTransaction();
+		EntityTransaction transaction = entityManager.getTransaction();
 
-		emp.setAge(43);
+		// saving/updating the employee objecting
+		//emp.setAge(42);
 
-		 transaction.begin();
+		transaction.begin();
 
-		 entityManager.persist(emp);
-		
-		 transaction.commit();
-		 entityManager.close();
-		 entityManagerFactory.close();
+		// saving/updating the employee objecting
+		//entityManager.persist(emp);
 
-		 System.out.println("Employee found"+emp);
+		entityManager.remove(emp
+		);
+		transaction.commit();
+		entityManager.close();
+		entityManagerFactory.close();
 
-
+		System.out.println("Employee found" + emp);
 
 	}
 
