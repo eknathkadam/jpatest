b273cf0776a5dc075431bf09e2218e94d2079349
Eknath Kadam <eknathkadam@gmail.com>
2022-05-22 17:08:42 +0530

    Retrieve and update of CRUD using JPAEntity manager

--- src/main/java/com/webrich/jpa/JpatestApplication.java
+++ src/main/java/com/webrich/jpa/JpatestApplication.java
@@ -1,7 +1,5 @@
 package com.webrich.jpa;
 
-import java.util.Date;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -13,37 +11,34 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JpatestApplication {
 
-	
 	public static void main(String[] args) {
 		
 		SpringApplication.run(JpatestApplication.class, args);
 
-		Employee emp1 = new Employee(1, "eknath", 42, "610553594", new Date(),EmployeeType.CONTRACTOR);
+		// Employee emp1 = new Employee(1, "eknath", 42, "610553594", new Date(),EmployeeType.CONTRACTOR);
 		
-		Employee emp2 = new Employee(2, "Girish", 28, "610553595",new Date(),EmployeeType.FULL_TIME);
-		Employee emp3 = new Employee(3, "Nirav", 45, "610553596",new Date(),EmployeeType.PAYROLL_EXEMPT);
+		// Employee emp2 = new Employee(2, "Girish", 28, "610553595",new Date(),EmployeeType.FULL_TIME);
+		// Employee emp3 = new Employee(3, "Nirav", 45, "610553596",new Date(),EmployeeType.PAYROLL_EXEMPT);
 
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myApp");
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		EntityTransaction transaction = entityManager.getTransaction();
 
+		Employee emp = entityManager.find(Employee.class, 1);
 
+		 EntityTransaction transaction = entityManager.getTransaction();
 
+		emp.setAge(43);
 
-		transaction.begin();
+		 transaction.begin();
 
-		entityManager.persist(emp1);
-		entityManager.persist(emp2);
-		entityManager.persist(emp3);
+		 entityManager.persist(emp);
 		
-		transaction.commit();
+		 transaction.commit();
 
-		System.out.println("Transaction commited");
+		 System.out.println("Employee found"+emp);
 
 
 
 	}
-
-	
 
 }
